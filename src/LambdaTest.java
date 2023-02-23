ef3f9baed46b0faac71332ef2a7ac0b5ff1b4eba
gleb <glebnsk@gmail.com>
2023-02-23 16:37:52 +0700

    добавил лямбду

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -1,16 +1,34 @@
 public class LambdaTest {
     public static void main(String[] args) {
-        System.out.println("Test lambda");
-        Operationsble oper;
         int a=-10;
         int b = 2;
         int c=30;
 
+        System.out.println("Test lambda");
+// заводим переменную типа функциональный интерфейс.
+        Operationsble oper;
+// согласно декларации интерфейс принимает два целых значения и возврящает одно целое значение
+// в следующей строке пишем чего именно делается с полученными числами
         oper = (x,y) -> a+x-b-y;
-
+        System.out.println(oper.calc(10,12));
+//при необходимости формулу вычисления можем поменять
+        oper = (x,y) -> a*x-b*y+c;
         System.out.println(oper.calc(10,12));
 
+// класс SquareEquation содержит значения коэффициентов, которые задаются через конструктор при создании класса и
+// интерфейс solve который по умолчанию является получением значения квадратного техчлена
+// согласно декларации интерфейса он получает одно число и возвращает одно число
+        SquareEquation squareEquation = new SquareEquation(-2,3,10);
+        for (int i = -10; i < 10; i++) {
+            System.out.printf("%.1f,\t",squareEquation.solve.calc((double)i));
+        }
+//точно также как в предыдущем примре формулу для вычисления можно изменить
+        System.out.println("\n===================");
+        squareEquation.solve = x1 -> squareEquation.a*x1;
 
+        for (int i = -10; i < 10; i++) {
+            System.out.printf("%.1f,\t",squareEquation.solve.calc((double)i));
+        }
     }
     @FunctionalInterface
     interface Operationsble {
--- src/SquareEquation.java
+++ src/SquareEquation.java
@@ -1,2 +1,26 @@
 public class SquareEquation {
+    public double a;
+    private double b;
+    private double c;
+    Solve solve = x1 -> a*x1*x1+b*x1+c;
+    public SquareEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public static void main(String[] args) {
+        SquareEquation s = new SquareEquation(-2,3,10);
+        double x=0;
+        System.out.println(s.solve.calc(10));
+        s.solve = x1 -> x1*x1;
+        System.out.println(s.solve.calc(10));
+    }
 }
+
+@FunctionalInterface
+interface Solve{
+    double calc(double x);
+}
+
+
